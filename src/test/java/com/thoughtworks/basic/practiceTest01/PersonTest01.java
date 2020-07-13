63113d1d216f5e1fa82509bee89cf69a30f9f49e
shijingwen <Sjw0123456>
2020-07-13 11:19:16 +0800

    practice01 done.

---
+++ src/test/java/com/thoughtworks/basic/practiceTest01/PersonTest01.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.basic.practiceTest01;
+
+import com.thoughtworks.basic.Dependency;
+import com.thoughtworks.basic.HelloWorld;
+import com.thoughtworks.basic.practice01.Person;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest01 {
+    @Test
+    public void person01_should_return_string() {
+        //given
+        Person person=new Person();
+
+        //when
+        String actual = person.introduce();
+
+        //then
+        assertEquals(actual,"My name is Tom. I am 21 years old.");
+    }
+}
